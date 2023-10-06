b78cf9fc094d3c9223e668aab616d37132c685be
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2023-10-05 23:35:59 -0500

    Update 343. Integer Break.java

--- 343. Integer Break.java
+++ 343. Integer Break.java
@@ -1,11 +1,14 @@
 class Solution {
     public int integerBreak(int n) {
+        if (n <= 2) return 1;
         int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
         Arrays.fill(dp, Integer.MIN_VALUE);
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j < i; j++) {
+        for (int i = 3; i <= n; i++) {
+            for (int j = 2; j <= 3; j++) {
                 dp[i] = Math.max(dp[i], j * (i-j));
                 dp[i] = Math.max(dp[i], j * dp[i-j]);
             }
