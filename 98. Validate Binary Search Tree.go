288285b3d26fa0e3d3649c8b700583a19f5de6a9
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2023-12-07 11:12:20 -0600

    Create 98. Validate Binary Search Tree.go

---
+++ 98. Validate Binary Search Tree.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+    var stack []*TreeNode
+    previous := math.MinInt64
+    for root != nil || len(stack) > 0 {
+        for root != nil {
+            stack = append(stack, root)
+            root = root.Left
+        }
+        root = stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        if root.Val <= previous {
+            return false
+        }
+        previous = root.Val
+        root = root.Right
+    }
+    return true
+}
