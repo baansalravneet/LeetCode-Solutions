455eeb9309327936325be34874da835ed4285a8a
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2024-01-27 17:02:19 -0600

    1410

---
+++ 1410. HTML Entity Parser.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String entityParser(String text) {
+        return text
+                .replace("&apos;", "'")
+                .replace("&quot;", "\"")
+                .replace("&gt;", ">")
+                .replace("&lt;", "<")
+                .replace("&frasl;", "/")
+                .replace("&amp;", "&");
+    }
+}
