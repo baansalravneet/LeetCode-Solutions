7780ad40b3949130916de324473d44049595bcfd
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2024-03-04 18:21:16 -0600

    1750

---
+++ 1750. Minimum Length of String After Deleting Similar Ends.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumLength(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) break;
+            char c = s.charAt(left);
+            while (left <= right && s.charAt(left) == c) left++;
+            while (left <= right && s.charAt(right) == c) right--;
+        } 
+        return right - left + 1;
+    }
+}
