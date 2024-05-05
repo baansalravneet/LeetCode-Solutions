04c8d1692b483b2541b58f7a5350049baf1703f2
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2024-05-04 22:25:15 -0500

    237

--- 237. Delete Node in a Linked List.java
+++ 237. Delete Node in a Linked List.java
@@ -9,11 +9,7 @@
 class Solution {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
-        if (node.next.next == null) {
-            node.next = null;
-            return;
-        }
-        deleteNode(node.next);
+        node.next = node.next.next;
     }
 }
 
