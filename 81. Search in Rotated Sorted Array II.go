9100bd632e14559b546b847efd64c6617f34b59e
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2024-07-16 03:02:29 -0500

    81

---
+++ 81. Search in Rotated Sorted Array II.go
@@ -0,0 +1,29 @@
+func search(nums []int, target int) bool {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return true
+		}
+		if nums[left] == nums[mid] && nums[left] == nums[right] {
+			left++
+			right--
+			continue
+		}
+		if nums[left] <= nums[mid] {
+			if target <= nums[mid] && target >= nums[left] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target >= nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return false
+}
+
