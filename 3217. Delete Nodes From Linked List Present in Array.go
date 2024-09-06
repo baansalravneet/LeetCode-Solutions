825a3bda1dcf7cef8041365999ff9cc1b76b4a93
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2024-09-05 20:15:32 -0500

    3217

--- 3217. Delete Nodes From Linked List Present in Array.go
+++ 3217. Delete Nodes From Linked List Present in Array.go
@@ -6,21 +6,20 @@
  * }
  */
 func modifiedList(nums []int, head *ListNode) *ListNode {
-	set := make(map[int]bool)
-	for _, v := range nums {
-		set[v] = true
-	}
-	newList := &ListNode{0, head}
-	pointer := newList
-	for head != nil {
-		for head != nil && set[head.Val] {
-			head = head.Next
-		}
-		pointer.Next = head
-		pointer = pointer.Next
-		if head != nil {
-			head = head.Next
-		}
-	}
-	return newList.Next
+    set := make(map[int]bool)
+    for _, v := range nums {
+        set[v] = true
+    }
+    newHead := &ListNode{}
+    pointer := newHead
+    for head != nil {
+        if !set[head.Val] {
+            pointer.Next = head
+            pointer = pointer.Next
+        }
+        head = head.Next
+    }
+    pointer.Next = nil
+    return newHead.Next
 }
+
--- 3217. Delete Nodes From Linked List Present in Array.java
+++ 3217. Delete Nodes From Linked List Present in Array.java
@@ -10,18 +10,18 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
+        ListNode newHead = new ListNode();
         Set<Integer> set = new HashSet<>();
         for (int i : nums) set.add(i);
-        ListNode newHead = new ListNode(0, head);
         ListNode pointer = newHead;
         while (head != null) {
-            while (head != null && set.contains(head.val)) {
-                head = head.next;
+            if (!set.contains(head.val)) {
+                pointer.next = head;
+                pointer = pointer.next;
             }
-            pointer.next = head;
-            if (head != null) head = head.next;
-            pointer = pointer.next;
+            head = head.next;
         }
+        pointer.next = null;
         return newHead.next;
     }
 }
