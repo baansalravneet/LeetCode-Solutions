958ee9eca306746b8b8c9f667d6a3c044024cd13
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2024-10-23 08:22:04 -0500

    2641

--- 2641. Cousins in Binary Tree II.java
+++ 2641. Cousins in Binary Tree II.java
@@ -15,39 +15,30 @@
  */
 class Solution {
     public TreeNode replaceValueInTree(TreeNode root) {
-        Deque<TreeNode> level = new ArrayDeque<>();
-        level.addLast(root);
-        List<Integer> levelSums = new ArrayList<>();
-        while (!level.isEmpty()) {
-            int size = level.size();
-            int sum = 0;
+        Deque<TreeNode> queue = new LinkedList<>();
+        int previous = root.val;
+        queue.addLast(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int currentSum = 0;
             while (size-- > 0) {
-                TreeNode current = level.pollFirst();
-                sum += current.val;
-                if (current.left != null) level.addLast(current.left);
-                if (current.right != null) level.addLast(current.right);
-            }
-            levelSums.add(sum);
-        }
-        int currentLevel = 0;
-        level.addLast(root);
-        while (!level.isEmpty()) {
-            int size = level.size();
-            while (size-- > 0) {
-                TreeNode current = level.pollFirst();
-                if (current.left != null) level.addLast(current.left);
-                if (current.right != null) level.addLast(current.right);
-                if (currentLevel < levelSums.size()-1) {
-                    int cousinSum = levelSums.get(currentLevel+1);
-                    if (current.left != null) cousinSum -= current.left.val;
-                    if (current.right != null) cousinSum -= current.right.val;
-                    if (current.left != null) current.left.val = cousinSum;
-                    if (current.right != null) current.right.val = cousinSum;
+                TreeNode current = queue.pollFirst();
+                current.val = previous - current.val;
+                int leftRightSum = 0;
+                if (current.left != null) {
+                    leftRightSum += current.left.val;
+                    queue.addLast(current.left);
                 }
+                if (current.right != null) {
+                    leftRightSum += current.right.val;
+                    queue.addLast(current.right);
+                }
+                if (current.left != null) current.left.val = leftRightSum;
+                if (current.right != null) current.right.val = leftRightSum;
+                currentSum += leftRightSum;
             }
-            currentLevel++;
+            previous = currentSum;
         }
-        root.val = 0;
         return root;
     }
 }
